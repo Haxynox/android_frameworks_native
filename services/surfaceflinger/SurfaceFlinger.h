da03e2f81c45a81783f9caf7beb1c23b020c335d
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-12-14 16:27:25 +0530

    sf: Workaround for swapBuffers bug on exynos4
    Conflicts:
    	services/surfaceflinger/SurfaceFlinger.cpp
    	services/surfaceflinger/SurfaceFlinger.h

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -134,6 +134,10 @@ public:
         return *mRenderEngine;
     }
 
+#ifdef SWAP_BUFFERS_WORKAROUND
+    int getNumVisibleRegions();
+#endif
+
 private:
     friend class Client;
     friend class DisplayEventConnection;
